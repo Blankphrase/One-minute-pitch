5a866406fd695bfdd19e29bbed560b1ba8559b48
Blankphrase <ckasera6486@gmail.com>
2018-06-27 11:45:04 +0300

    importing migrate

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ from app import create_app, db
 from flask_script import Manager, Server
 from app.models import User, Role
 from config import Config
+from flask_migrate import Migrate, MigrateCommand
 
 
 app = create_app('production')
