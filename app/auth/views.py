9fe40820379ce369eef16b202da2765b5fcbe7cd
Blankphrase <ckasera6486@gmail.com>
2018-06-27 13:11:55 +0300

    creation of registration function

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,22 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
+from ..models import User
+from .forms import RegistrationForm
 from . import auth
+from .. import db
 
 
-@auth.route('/login', methods=['POST', 'GET'])
+@auth.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html', registration_form=form)
+
+@auth.route('/login', methods=["GET", "POST"])
 def do_admin_login():
     if request.form['password'] == 'password' and request.form['username'] == 'admin':
         session['logged_in'] = True
