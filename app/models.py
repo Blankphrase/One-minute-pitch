b40f8e8efe916fb24d1de696957c2ec84862e264
Blankphrase <ckasera6486@gmail.com>
2018-06-27 14:10:38 +0300

    creation of user profile

--- app/models.py
+++ app/models.py
@@ -6,9 +6,14 @@ from . import login_manager
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
+
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True, index=True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
 
     @property
