9c7dadaf000ace88a31194da4f5ee66d1ce595a0
Blankphrase <ckasera6486@gmail.com>
2018-06-27 16:01:07 +0300

    debugging errors

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms.validators import Required
-from wtforms import SubmitField, TextAreaField
+from wtforms.validators import Required,Email
+from wtforms import SubmitField, TextAreaField, StringField,ValidationError
+from ..models import User
+
+
+class ReviewForm(FlaskForm):
+
+    title = StringField('Review title', validators=[Required()])
+    review = TextAreaField('Movie review', validators=[Required()])
+    submit = SubmitField('Submit')
+
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.', validators=[Required()])
--- app/main/views.py
+++ app/main/views.py
@@ -2,16 +2,21 @@ from flask import Flask, flash, redirect, render_template, request, session, abo
 from . import main 
 from flask_login import login_required
 from .forms import ReviewForm, UpdateProfile
+from .. import db
+from ..models import Review, User
 
 @main.route('/')
-def home():
-    if not session.get('logged_in'):
-        return render_template('login.html')
-    else:
-        return "Hello Boss!"
+def index():
+    """
+    View root page function that returns the index page and its data
+    """
 
+    title = 'Home - Welcome to The Pitch website'
+
+    return render_template('index.html', title=title)
 
 @main.route('/user/<uname>')
+@login_required
 def profile(uname):
     user = User.query.filter_by(username=uname).first()
 
@@ -40,7 +45,21 @@ def update_profile(uname):
 
     return render_template('profile/update.html', form=form)
 
+
 @main.route('/pitch/review/new/<int:id>', methods=['GET', 'POST'])
 @login_required
 def new_review(id):
-    pass
+
+    form = ReviewForm()
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+
+        # new_review = Review(pitch.id, title, pitch.poster, review)
+        # new_review.save_review()
+
+        # return redirect(url_for('.movie', id=pi.id))
+
+    # title = f'{movie.title} review'
+    return render_template('new_review.html', review_form=form)
