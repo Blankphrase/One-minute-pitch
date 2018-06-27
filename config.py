cb4a534e17ddff354f382a4aaea090135787e1fd
Blankphrase <ckasera6486@gmail.com>
2018-06-27 11:34:16 +0300

    setting debugging to true

--- config.py
+++ config.py
@@ -5,13 +5,15 @@ class Config:
     '''
     general configuration
     '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://blankphrase:password@localhost/pitch'
 
 class ProdConfig(Config):
     '''
     production configuration child class
     '''
-    
+    pass
+
 class DevConfig(Config):
     '''
     development configuration child class
