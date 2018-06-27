196b4dc34a9b34b2407a76f000749624f1d5d6a9
Blankphrase <ckasera6486@gmail.com>
2018-06-27 14:54:33 +0300

    creation of update profile route

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
 from . import main 
 from flask_login import login_required
+from .forms import ReviewForm, UpdateProfile
 
 @main.route('/')
 def home():
@@ -18,6 +19,26 @@ def profile(uname):
         abort(404)
 
     return render_template("profile/profile.html", user=user)
+
+
+@main.route('/user/<uname>/update', methods=['GET', 'POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile', uname=user.username))
+
+    return render_template('profile/update.html', form=form)
 
 @main.route('/pitch/review/new/<int:id>', methods=['GET', 'POST'])
 @login_required
