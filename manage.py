d146d8e9b2a8ce5129cc2fe524382843476a9374
Blankphrase <ckasera6486@gmail.com>
2018-06-27 11:46:17 +0300

    define migrate

--- manage.py
+++ manage.py
@@ -10,6 +10,8 @@ app = create_app('production')
 manager = Manager(app)
 manager.add_command('server', Server)
 
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db,User = User, Role = Role )
