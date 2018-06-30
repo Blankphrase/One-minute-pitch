8d5c4e6dafb368a61abbe173f0ee55cac74b916b
Blankphrase <ckasera6486@gmail.com>
2018-06-30 13:03:34 +0300

    addition of reviews and categorical forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,19 +3,82 @@ from wtforms.validators import Required,Email
 from wtforms import SubmitField, TextAreaField, StringField,ValidationError
 from ..models import User
 
-
-class ReviewForm(FlaskForm):
-
-    username = StringField('Username', validators=[Required()])
-    review = TextAreaField('Pitch review', validators=[Required()])
-    submit = SubmitField('Submit')
-
-
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.', validators=[Required()])
     submit = SubmitField('Submit')
 
-class PitchForm(FlaskForm):
-    username = StringField('Username', validators = [Required()])
-    post = TextAreaField('Pitch', validators = [Required()])
+
+class GeneralForm(FlaskForm):
+    post = StringField('Title', validators=[Required()])
+    body = TextAreaField('Post', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class GeneralReviewForm(FlaskForm):
+    review = StringField('Review: ', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class SaleForm(FlaskForm):
+    post = StringField('Title', validators=[Required()])
+    body = TextAreaField('Post', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class SaleReviewForm(FlaskForm):
+    review = StringField('Review: ', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class SeductionForm(FlaskForm):
+    post = StringField('Title', validators=[Required()])
+    body = TextAreaField('Post', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class SeductionReviewForm(FlaskForm):
+    review = StringField('Review: ', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class MusicForm(FlaskForm):
+    post = StringField('Title', validators=[Required()])
+    body = TextAreaField('Post', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class MusicReviewForm(FlaskForm):
+    review = StringField('Review: ', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class ProjectForm(FlaskForm):
+    post = StringField('Title', validators=[Required()])
+    body = TextAreaField('Post', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class ProjectReviewForm(FlaskForm):
+    review = StringField('Review: ', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class InterviewForm(FlaskForm):
+    post = StringField('Title', validators=[Required()])
+    body = TextAreaField('Post', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class InterviewReviewForm(FlaskForm):
+    review = StringField('Review': ', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class AdvertisementForm(FlaskForm):
+    post = StringField('Title', validators=[Required()])
+    body = TextAreaField('Post', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class AdvertisementReviewForm(FlaskForm):
+    review = StringField('Review: ', validators=[Required()])
     submit = SubmitField('Submit')
